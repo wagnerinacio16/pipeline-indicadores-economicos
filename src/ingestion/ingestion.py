39fe2ee90d6f9851c3b134e27cfd7c129c29e9f1
Wagner Oliveira <wagner.inacio16@outlook.com>
2025-07-12 22:29:01 -0300

    camada bronze finalizada

--- src/ingestion/ingestion.py
+++ src/ingestion/ingestion.py
@@ -1,11 +1,3 @@
-
-import os
-import sys
-
-project_root = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
-if project_root not in sys.path:
-    sys.path.append(project_root)
-
 from config.utils import *
 
 
